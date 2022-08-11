4a67e09632fee7bebffca81c256c18fdf081cb67
root <root@ip-172-31-93-110.ec2.internal>
2022-08-11 18:31:01 +0000

    added output value to pring ALB arn

---
+++ variable.tf
@@ -0,0 +1,98 @@
+# Region 
+variable "Region" {
+    default = "us-east-1"
+    description = "Region"
+    type = string 
+}
+
+#VPC CIDR 
+variable "vpc-cidr" {
+    default = "10.0.0.0/16"
+    description = "vpc cidr block"
+    type = string 
+}
+
+#Public SubNet
+variable "Public-Subnet1-cidr" {
+    default = "10.0.1.0/24"
+    description = "Public Subnet 1 CIDR block"
+    type = string
+}
+
+variable "Public-Subnet2-cidr" {
+    default = "10.0.2.0/24"
+    description = "Public Subnet 2 CIDR block"
+    type = string
+}
+
+#Private SubNet
+variable "Private-Subnet1-cidr" {
+    default = "10.0.3.0/24"
+    description = "Private Subnet 1 CIDR block"
+    type = string
+}
+
+variable "Private-Subnet2-cidr" {
+    default = "10.0.4.0/24"
+    description = "Private Subnet 2 CIDR block"
+    type = string
+}
+
+#Availability Zone
+variable "az1" {
+    default = "us-east-1a"
+    description = "Availability Zone (us-east-1a)"
+    type = string
+}
+
+variable "az2" {
+    default = "us-east-1b"
+    description = "Availability Zone (us-east-1b)"
+    type = string
+}
+
+#Security Groups
+locals {
+   Internet_ingress_rules = [{
+      port        = 22
+      description = "Ingress rules for port 22"
+   },
+   {
+      port        = 80
+      description = "Ingree rules for port 80"
+   },
+   {
+      port        = 443
+      description = "Ingree rules for port 443"
+   }]
+}
+
+locals {
+   Internal_ingress_rules = [{
+      port        = 22
+      description = "Ingress rules for port 22"
+   },
+   {
+      port        = 80
+      description = "Ingree rules for port 80"
+   },
+   {
+      port        = 3306
+      description = "Ingree rules for port 3306"
+   }]
+}
+
+locals {
+   Bastian_ingress_rules = [{
+      port        = 22
+      description = "Ingress rules for port 22"
+   }]
+}
+
+#AMI
+variable "AMI" {
+   default = "ami-090fa75af13c156b4"
+   description = "AMI"
+    type = string
+}
+
